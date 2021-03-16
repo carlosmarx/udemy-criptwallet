e4265e98ab915799991824b3f8572aa55163d3f9
Carlos Marx <carlosmarx27@gmail.com>
2021-03-16 16:53:21 -0300

    Crud mining types

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,23 +6,31 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 puts "Cadastrando criptomoedas..."
-Coin.create!(
-  description: "Bitcoin",
-  acronym: "BTC",
-  url_image: "https://logospng.org/download/bitcoin/logo-bitcoin-1536.png"
-)
 
-Coin.create!(
-  description: "Ethereum",
-  acronym: "ETH",
-  url_image: "https://w7.pngwing.com/pngs/906/776/png-transparent-ethereum-blockchain-bitcoin-logo-see-you-there-angle-triangle-logo.png"
-)
+coins = 
+[
+  { 
+    description: "Bitcoin",
+    acronym: "BTC",
+    url_image: "https://logospng.org/download/bitcoin/logo-bitcoin-1536.png"
+  },
 
-Coin.create!(
-  description: "Dash",
-  acronym: "DASH",
-  url_image: "https://w7.pngwing.com/pngs/37/123/png-transparent-dash-bitcoin-cryptocurrency-digital-currency-logo-bitcoin-blue-angle-text.png"
-)
+  {
+    description: "Ethereum",
+    acronym: "ETH",
+    url_image: "https://w7.pngwing.com/pngs/906/776/png-transparent-ethereum-blockchain-bitcoin-logo-see-you-there-angle-triangle-logo.png"
+  },
+
+  {
+    description: "Dash",
+    acronym: "DASH",
+    url_image: "https://w7.pngwing.com/pngs/37/123/png-transparent-dash-bitcoin-cryptocurrency-digital-currency-logo-bitcoin-blue-angle-text.png"
+  }
+]
+
+coins.each do |coin|
+  Coin.find_or_create_by(coin)
+end
 
 
 puts "Criptomoedas cadastradas com sucesso!"
--- lib/tasks/dev.rake
+++ lib/tasks/dev.rake
@@ -1,6 +1,24 @@
 namespace :dev do
-  desc "TODO"
+  desc "Configura o ambiente de desenvolvimento."
   task setup: :environment do
+    if Rails.env.development?
+      spinner = TTY::Spinner.new("[:spinner] Apagando banco de dados...")
+      spinner.auto_spin
+
+      show_spinner("Apagando banco de dados", "Banco apagado com sucesso!") { %x(rails db:drop) }
+      show_spinner("Criando banco de dados", "Banco criado com sucesso!!") { %x(rails db:create) }
+      show_spinner("Executando migrations", "Migrations executadas com sucesso!") { %x(rails db:migrate) }
+      show_spinner("Populando banco de dados", "Banco de dados populado com sucesso!") { %x(rails db:seed) }
+      
+    else 
+      puts "Você não está em ambiente de desenvolvimento"
+    end
   end
 
+  def show_spinner(msg_start, msg_end)
+    spinner = TTY::Spinner.new("[:spinner] #{msg_start}...")
+    spinner.auto_spin
+    yield
+    spinner.success("(#{msg_end})")
+  end
 end
