ae614dba767dc37cf9f6a13e2e2ff8621f54d644
Carlos Marx <carlosmarx27@gmail.com>
2021-03-25 01:03:33 -0300

    Configure I18n pt-BR

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    # This cookie will be deleted when the user's browser is closed.
+    # cookies[:curso] = "Curso de Ruby on Rails [COOKIE]"
+    # session[:curso] = "Curso de Ruby on Rails da UDEMY [SESSION]"
   end
 end
