09083dae6a2884b1ee27a26b90ac22d439345241
Carlos Marx <carlosmarx27@gmail.com>
2021-03-26 13:56:19 -0300

    Translate index welcome

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,17 @@
 const { environment } = require('@rails/webpacker')
 
+const customConfig = require('./custom');
+
+const webpack = require('webpack')
+
+environment.plugins.append('Provide',
+    new webpack.ProvidePlugin({
+        $: 'jquery/src/jquery',
+        jQuery: 'jquery/src/jquery',
+        notify: 'notify-js-legacy/notify'
+    })
+)
+
+environment.config.merge(customConfig);
+
 module.exports = environment
